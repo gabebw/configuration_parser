49ec20258932bc03a3e00ee0d4a6167b011a49f6
Gabe Berke-Williams <gabebw@gabebw.com>
2014-05-23 11:18:46 -0400

    The parser can parse arbitrarily-nested configuration

--- lib/configuration_parser.rb
+++ lib/configuration_parser.rb
@@ -1,7 +1,29 @@
 require "configuration_parser/version"
 
 module ConfigurationParser
-  def self.parse
+  def self.parse(string)
+    proxy = Proxy.new
+    proxy.instance_eval(string)
+    proxy.hash
+  end
 
+  class Proxy
+    def initialize
+      @hash = {}
+    end
+
+    attr_reader :hash
+
+    private
+
+    def set(value, other_value)
+      @hash[value] = other_value
+    end
+
+    def configure(nest, &block)
+      nester = Proxy.new
+      nester.instance_eval(&block)
+      @hash[nest] = nester.hash
+    end
   end
 end
--- spec/configuration_parser_spec.rb
+++ spec/configuration_parser_spec.rb
@@ -2,6 +2,55 @@ describe ConfigurationParser, '.parse' do
   it 'parses `set :foo, "bar"`' do
     result = ConfigurationParser.parse('set :foo, "bar"')
 
-    expect(result).to eq({ foo: bar })
+    expect(result).to eq({ foo: "bar" })
+  end
+
+  it 'parses multiple lines of `set :foo, "bar"`' do
+    configuration = <<-EOS
+      set :foo, "bar"
+      set :other, 3
+    EOS
+
+    result = ConfigurationParser.parse(configuration)
+
+    expect(result).to eq({ foo: "bar", other: 3 })
+  end
+
+  it 'can nest configuration' do
+    configuration = <<-EOS
+      configure :blog do
+        set :root, "/blog"
+      end
+    EOS
+
+    result = ConfigurationParser.parse(configuration)
+
+    expect(result).to eq({ blog: { root: "/blog" } })
+  end
+
+  it 'can nest configuration arbitrarily deep' do
+    configuration = <<-EOS
+      configure :blog do
+        configure :second_level do
+          set :hello, :there
+          configure :inception do
+            set :park, :place
+          end
+        end
+      end
+    EOS
+
+    result = ConfigurationParser.parse(configuration)
+
+    expect(result).to eq({
+      blog: {
+        second_level: {
+          hello: :there,
+          inception: {
+            park: :place
+          }
+        }
+      }
+    })
   end
 end
